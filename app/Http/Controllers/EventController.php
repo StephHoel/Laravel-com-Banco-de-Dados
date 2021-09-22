21ff000c65709d72b583c68f5a4707c71c99d221
StephHoel <steph.hoel@gmail.com>
2021-09-22 13:32:49 -0300

    Aula 19 - Adicionando recurso items na hora de criar um evento e mostrando as opções de infra(items)

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -37,14 +37,15 @@ class EventController extends Controller
         $event->city = $request->city;
         $event->private = $request->private;
         $event->description = $request->description;
+        $event->items = $request->items;
 
         //Image Upload
         if($request->hasFile('image') && $request->file('image')->isValid()){
-            $requestImage -> $request->image;
+            $requestImage = $request->image;
 
             $extension = $requestImage->extension();
 
-            $imageName = md5($requestImage->getClientOriginalName() . strotime("now")) . "." . $extension;
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
 
             $request->image->move(public_path('img/events'), $imageName);
 
