6ba2f48860c48f7336fd34bd5834c32096e020ea
StephHoel <steph.hoel@gmail.com>
2021-10-07 11:49:45 -0300

    Adicionando novos arquivos

---
+++ resources/views/steph/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('title', 'Home | Steph Hoel')
+
+@section('content')
+
+<div id="events-container" class="col-md-12">
+    Página Inicial Home
+</div>
+
+@endsection
---
+++ resources/views/steph/videos.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('title', 'Home | Steph Hoel')
+
+@section('content')
+
+<div id="events-container" class="col-md-12">
+    Página Inicial Vídeos
+</div>
+
+@endsection
